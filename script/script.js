c4774baee53b5eff660f06a665f008fc0335cab9
belkooo103 <belminzola@gmail.com>
2025-08-06 14:23:14 +0200

    Finished code for deciding, add count for wins, add function to play 5 times

--- script/script.js
+++ script/script.js
@@ -1,23 +1,36 @@
 function getUserInput(){
-    return prompt("Please input either Rock, Paper or Scissor.");
+    return prompt("Please input either Rock, Paper or Scissors.");
 }
 
+// 1 - rock, 2 - paper, 3 - scissor
 function generateComputerInput(){
    return Math.floor(Math.random() * 3) + 1;
 }
-// 1 - rock, 2 - paper, 3 - scissor
-let userInput = String(getUserInput()).toLowerCase();
-const computerInput = generateComputerInput();
-console.log(computerInput);
-function playGame(user, computer){
-    if((userInput == 'rock' && computerInput == 1) || (userInput == 'paper' && computer == 2) || (userInput == 'scissor' && computerInput == 3)){
+
+let userWinCount = parseInt(0);
+let computerWinCount = parseInt(0);
+function playRound(){
+    
+    let userInput = String(getUserInput()).toLowerCase();
+    const computerInput = generateComputerInput();
+    if((userInput == 'rock' && computerInput == 1) || (userInput == 'paper' && computerInput == 2) || (userInput == 'scissors' && computerInput == 3)){
         console.log("DRAW!");
     }
-    else if((userInput == 'rock' && computerInput == 3) || (userInput == 'paper' && computerInput == 1) || (userInput == 'scissor' && computerInput == 1)){
+    else if((userInput == 'rock' && computerInput == 3) || (userInput == 'paper' && computerInput == 1) || (userInput == 'scissors' && computerInput == 1)){
             console.log("USER WINS!!")
+            userWinCount = userWinCount + 1;
+            console.log(`User: ${userWinCount} || Computer: ${computerWinCount}.` )
     }
     else{
         console.log("COMPUTER WINS!!")
+        computerWinCount++;
+         console.log(`User: ${userWinCount} || Computer: ${computerWinCount}.` )
     }
+   
 }
-playGame(userInput, computerInput);
+function playGame(roundsNumber){
+   for(let i = 0; i < roundsNumber; i++){
+     playRound();
+   }
+}
+playGame(5);
