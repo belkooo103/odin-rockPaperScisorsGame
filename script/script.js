074b53923f0e49871c69c207ce37c0a35c3c4d48
belkooo103 <belminzola@gmail.com>
2025-08-06 14:03:19 +0200

    Made function that compare inputs

--- script/script.js
+++ script/script.js
@@ -1,6 +1,23 @@
 function getUserInput(){
-    return prompt("Please enter 1 for rock, 2 for paper, 3 for scissors.");
+    return prompt("Please input either Rock, Paper or Scissor.");
 }
-function getComputerInput(){
-    return Math.floor(Math.random * 3) + 1;
+
+function generateComputerInput(){
+   return Math.floor(Math.random() * 3) + 1;
 }
+// 1 - rock, 2 - paper, 3 - scissor
+let userInput = String(getUserInput()).toLowerCase();
+const computerInput = generateComputerInput();
+console.log(computerInput);
+function playGame(user, computer){
+    if((userInput == 'rock' && computerInput == 1) || (userInput == 'paper' && computer == 2) || (userInput == 'scissor' && computerInput == 3)){
+        console.log("DRAW!");
+    }
+    else if((userInput == 'rock' && computerInput == 3) || (userInput == 'paper' && computerInput == 1) || (userInput == 'scissor' && computerInput == 1)){
+            console.log("USER WINS!!")
+    }
+    else{
+        console.log("COMPUTER WINS!!")
+    }
+}
+playGame(userInput, computerInput);
